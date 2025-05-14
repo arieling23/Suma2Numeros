855ffca60a5ddc1bc01fa3782c5d42dd371451fa
arieling23 <123669897+arieling23@users.noreply.github.com>
2025-05-14 14:15:39 -0500

    Update Suma.py

--- Suma.py
+++ Suma.py
@@ -2,15 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/sumar", methods=["GET"])
+@app.route("/sumar")
 def sumar():
     try:
         a = float(request.args.get("a"))
         b = float(request.args.get("b"))
-        resultado = a + b
-        return jsonify({"resultado": resultado})
-    except (TypeError, ValueError):
+        return jsonify({"resultado": a + b})
+    except:
         return jsonify({"error": "Parámetros inválidos"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
